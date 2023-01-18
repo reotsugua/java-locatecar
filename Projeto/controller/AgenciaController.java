c78a609222fb3f14795fb35fd545483f92291027
Felipe Menezes <mfelipe758@gmail.com>
2023-01-18 20:54:18 -0300

    Criando classes: AgenciaController e AgenciaView

--- Projeto/controller/AgenciaController.java
+++ Projeto/controller/AgenciaController.java
@@ -1,43 +1,43 @@
-//package Projeto.controller;
-//
-//import Projeto.model.Agencia;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//
-//public class AgenciaController <T extends Agencia>{
-//    private List<T> agencias;
-//
-//    public AgenciaController() {
-//        if (agencias == null) {
-//            agencias = new ArrayList<>();
-//        }
-//    }
-//
-//    public void cadastrarAgencia(T agencia) {
-//        agencias.add(agencia);
-//    }
-//
-//    public List<T> listarAgencias() {
-//        return agencias;
-//    }
-//
-//    public List<Agencia> pesquisarAgencia(String nome){
-//        List<Agencia> agenciasEncontradas = new ArrayList<>();
-//        List<? extends Agencia> agencias = listarAgencias();
-//        for (int i = 0; i < agencias.size(); i++) {
-//            if (agencias.get(i).getNome().contains(nome)){
-//                agenciasEncontradas.add(agencias.get(i));
-//            }
-//        }
-//        return agenciasEncontradas;
-//    }
-//    public Agencia agenciaExistente(String nome){
-//        for ( Agencia agenciaExistente: agencias) {
-//            if (nome.equals(agenciaExistente.getNome()))
-//                return agenciaExistente;
-//        }
-//        return null;
-//    }
-//}
+package Projeto.controller;
+
+import Projeto.model.Agencia;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AgenciaController <T extends Agencia>{
+    private List<T> agencias;
+
+    public AgenciaController() {
+        if (agencias == null) {
+            agencias = new ArrayList<>();
+        }
+    }
+
+    public void cadastrarAgencia(T agencia) {
+        agencias.add(agencia);
+    }
+
+    public List<T> listarAgencias() {
+        return agencias;
+    }
+
+    public List<Agencia> pesquisarAgencia(String nome){
+        List<Agencia> agenciasEncontradas = new ArrayList<>();
+        List<? extends Agencia> agencias = listarAgencias();
+        for (int i = 0; i < agencias.size(); i++) {
+            if (agencias.get(i).getNome().contains(nome)){
+                agenciasEncontradas.add(agencias.get(i));
+            }
+        }
+        return agenciasEncontradas;
+    }
+    public Agencia agenciaExistente(String nome){
+        for ( Agencia agenciaExistente: agencias) {
+            if (nome.equals(agenciaExistente.getNome()))
+                return agenciaExistente;
+        }
+        return null;
+    }
+}
--- Projeto/view/AgenciaView.java
+++ Projeto/view/AgenciaView.java
@@ -1,71 +1,71 @@
-//package Projeto.view;
-//
-//import Projeto.controller.AgenciaController;
-//import Projeto.model.*;
-//import Projeto.util.ConsoleUIHelper;
-//
-//import java.util.List;
-//
-//public class AgenciaView {
-//    private static AgenciaController controller;
-//
-//    public AgenciaView() {
-//        if (controller == null) {
-//            controller = new AgenciaController();
-//        }
-//    }
-//
-//    public void cadastrarAgencia() {
-//
-//        Agencia agencia;
-//
-//        String nome = ConsoleUIHelper.askNoEmptyInput("Informe o nome da agência: ", 2);
-//        String endereco = ConsoleUIHelper.askNoEmptyInput("Informe o endereço da agência: ", 2);
-//
-//        Agencia agenciaExistente = controller.agenciaExistente(nome);
-//        while (agenciaExistente != null){
-//            System.out.println("Agência já cadastrada.");
-//            nome = ConsoleUIHelper.askNoEmptyInput("Informe o nome da agência: ", 2);
-//            agenciaExistente = controller.agenciaExistente(nome);
-//        }
-//
-//        agencia = new Agencia(nome, endereco);
-//        controller.cadastrarAgencia(agencia);
-//    }
-//
-//    public void listarAgencias() {
-//        List<? extends Agencia> agencias = controller.listarAgencias();
-//
-//        for(int i = 0; i < agencias.size(); i++) {
-//            System.out.println("[" + (i+1) + "] " + agencias.get(i));
-//        }
-//    }
-//
-//    public void alterarAgencia() {
-//
-//        listarAgencias();
-//
-//        List<? extends Agencia> agencias = controller.listarAgencias();
-//
-//        String escolha = ConsoleUIHelper.askNoEmptyInput("Selecione a agência através do índice.", 2);
-//
-//        for (int i = 0; i < agencias.size(); i++) {
-//            if (Integer.valueOf(escolha) == (i+1)) {
-//                System.out.println("Agência selecionada:");
-//                System.out.println("[" + (i+1) + "] " + agencias.get(i));
-//                agencias.remove(i);
-//                cadastrarAgencia();
-//                System.out.println(agencias.get(i));
-//                System.out.println("Agência alterada com sucesso!");
-//            }
-//        }
-//    }
-//    public void pesquisarAgencia(){
-//        String nome = ConsoleUIHelper.askNoEmptyInput("Informe o nome da agência: ", 2);
-//        List<Agencia> agenciasEncontradas = controller.pesquisarAgencia(nome);
-//        for (Agencia agencia: agenciasEncontradas) {
-//            System.out.println(agencia);
-//        }
-//
-//    }
-//}
+package Projeto.view;
+
+import Projeto.controller.AgenciaController;
+import Projeto.model.*;
+import Projeto.util.ConsoleUIHelper;
+
+import java.util.List;
+
+public class AgenciaView {
+    private static AgenciaController controller;
+
+    public AgenciaView() {
+        if (controller == null) {
+            controller = new AgenciaController();
+        }
+    }
+
+    public void cadastrarAgencia() {
+
+        Agencia agencia;
+
+        String nome = ConsoleUIHelper.askNoEmptyInput("Informe o nome da agência: ", 2);
+        String endereco = ConsoleUIHelper.askNoEmptyInput("Informe o endereço da agência: ", 2);
+
+        Agencia agenciaExistente = controller.agenciaExistente(nome);
+        while (agenciaExistente != null){
+            System.out.println("Agência já cadastrada.");
+            nome = ConsoleUIHelper.askNoEmptyInput("Informe o nome da agência: ", 2);
+            agenciaExistente = controller.agenciaExistente(nome);
+        }
+
+        agencia = new Agencia(nome, endereco);
+        controller.cadastrarAgencia(agencia);
+    }
+
+    public void listarAgencias() {
+        List<? extends Agencia> agencias = controller.listarAgencias();
+
+        for(int i = 0; i < agencias.size(); i++) {
+            System.out.println("[" + (i+1) + "] " + agencias.get(i));
+        }
+    }
+
+    public void alterarAgencia() {
+
+        listarAgencias();
+
+        List<? extends Agencia> agencias = controller.listarAgencias();
+
+        String escolha = ConsoleUIHelper.askNoEmptyInput("Selecione a agência através do índice.", 2);
+
+        for (int i = 0; i < agencias.size(); i++) {
+            if (Integer.valueOf(escolha) == (i+1)) {
+                System.out.println("Agência selecionada:");
+                System.out.println("[" + (i+1) + "] " + agencias.get(i));
+                agencias.remove(i);
+                cadastrarAgencia();
+                System.out.println(agencias.get(i));
+                System.out.println("Agência alterada com sucesso!");
+            }
+        }
+    }
+    public void pesquisarAgencia(){
+        String nome = ConsoleUIHelper.askNoEmptyInput("Informe o nome da agência: ", 2);
+        List<Agencia> agenciasEncontradas = controller.pesquisarAgencia(nome);
+        for (Agencia agencia: agenciasEncontradas) {
+            System.out.println(agencia);
+        }
+
+    }
+}
